8d42fb4d1988678228dd70cd2a56a9eff96fb88f
Dyego <dyego.villar@run2biz.com>
2026-04-04 00:07:38 -0300

    Performing some further configurations on the 2D array function.

--- main.c
+++ main.c
@@ -6,12 +6,19 @@
 #include "src/class4_Structs/createBooks/class4_createBook.h"
 #include "src/class5_Loops/while/class5_while.h"
 #include "src/class5_Loops/for/class5_for.h"
+#include "src/class6_2D_Arrays/class6.h"
 
 
 int main() {
 
-    int arr[] = {50, 60, 70, 80, 90};
-    loopingArray(arr, 5);
+    int arr[][3] = {
+        {50, 60, 70},
+        {60, 50, 70},
+        {70, 40, 70},
+        {80, 30, 70},
+        {80, 20, 70},
+    };
+    print2DArray(arr, 5, 3);
 
     return 0;
 }
--- src/class6_2D_Arrays/print2DAray_2D.c
+++ src/class6_2D_Arrays/print2DAray_2D.c
@@ -3,8 +3,9 @@
 
 void print2DArray(int arr[][3], int rows, int cols) {
     for (int i = 0; i < rows; i++) {
+        printf("Iterating through the array: [%d]\n", i);
         for (int j = 0; j < cols; j++) {
-            printf("%d ", arr[i][j]);
+            printf("%d, ", arr[i][j]);
         }
         printf("\n");
     }
